ed5cf82c94898a9da2ac27453a966bbc3f6a89d1
junjora <kropotov.egor.stanislavovich@yandex.ru>
2020-12-03 22:51:30 +0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,8 +2,8 @@ import Arrays.java;
 
 class BubbleSort{
   
-  public static void bubbleSort(int arr[]){
-    n=arr.length;
+  public static void bubbleSort1(int arr[]){
+    int n=arr.length;
     for(int i=0; i < n; i++){
       for(int j=0; j < n-i-1; j++){
         if(arr[i] > arr[i+1]){
@@ -14,15 +14,39 @@ class BubbleSort{
       }
     }
   }
-
+  
+  //other realization
+  public static void bubbleSort2(int arr[]){
+    int n=arr.length;
+    boolean isSort=false;
+    while(!isSort){
+      isSort=true;
+      for(int i=0; i < n; i++){
+          if(arr[i] > arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            isSort=false;
+          }
+        }
+      }
+    }
+  }
+  
   public static void main(String args[]) { 
     BubbleSort bs = new BubbleSort();
     Arrays a = new Arrays();
     System.out.println("Исходный массив");
-    int arr[]=a.createMassive(7,100);
-    a.printArray(arr);
+    int arr1[]=a.createMassive(7,100);
+    a.printArray(arr1);
     System.out.println("Отсортированный массив");
-    a.measureTime(()->bs.bubbleSort(arr));
-    a.printArray(arr);
+    a.measureTime(()->bs.bubbleSort(arr1));
+    a.printArray(arr1);
+    System.out.println("Исходный массив");
+    int arr2[]=a.createMassive(7,100);
+    a.printArray(arr2);
+    System.out.println("Отсортированный массив");
+    a.measureTime(()->bs.bubbleSort(arr2));
+    a.printArray(arr2);
   }
 }
