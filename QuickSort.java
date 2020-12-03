2ed38f1588f89f349053b07ba5bb479b65da4c37
junjora <kropotov.egor.stanislavovich@yandex.ru>
2020-12-03 17:23:52 +0500

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,43 @@
+import Arrays.java;
+
+class QuickSort{
+
+  public static int partition(int arr[], int low, int high){
+    int pivot = arr[high]; //choose pivot
+    int i = (low-1); //index of smaller element
+    for(int j=low; j<high; j++){
+         // If current element is smaller than the pivot
+         if(arr[j] < pivot){
+             i++;
+             int temp = arr[i];
+             arr[i] = arr[j];
+             arr[j] = temp;
+         }
+    }
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+    return i+1;
+  }
+
+  void sortQuick(int arr[], int low, int high){
+      if(low < high){
+          //pi is partitioning index, arr[pi] is now at right place
+          int pi = partition(arr, low, high);
+          // Recursively sort elements before partition and after partition 
+          sortQuick(arr, low, pi-1);
+          sortQuick(arr, pi+1, high);
+      }
+   }
+   
+   public static void main(String args[]) { 
+    QuickSort qs = new QuickSort();
+    Arrays a = new Arrays();
+    System.out.println("Исходный массив");
+    int arr[]=a.createMassive(7,100);
+    a.printArray(arr);
+    System.out.println("Отсортированный массив");
+    a.measureTime(()->qs.sortQuick(arr));
+    a.printArray(arr);
+  }
+}
